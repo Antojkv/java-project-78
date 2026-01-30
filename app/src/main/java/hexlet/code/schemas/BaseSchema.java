e8656060d8ae66b693728e8a5aec1aa4bbeff008
Антон Кузянин <marswaitsfor@gmail.com>
2026-01-31 01:47:11 +0300

    fix problems10

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -3,7 +3,7 @@ package hexlet.code.schemas;
 public abstract class BaseSchema<T> {
     protected boolean required = false;
 
-    @Deprecated
+    @SuppressWarnings("checkstyle:DesignForExtension")
     public BaseSchema<T> required() {
         this.required = true;
         return this;
